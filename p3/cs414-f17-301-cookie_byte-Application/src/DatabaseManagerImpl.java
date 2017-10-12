78303e8683f5640e6742e3edc0cca4c4faaf8a47
torry brelsford <torrybr@rams.colostate.edu>
2017-10-12 16:52:12 -0600

    created an implementataion and interface for interacting with the database

---
+++ p3/cs414-f17-301-cookie_byte-Application/src/DatabaseManagerImpl.java
@@ -0,0 +1,24 @@
+import java.net.URL;
+
+public class DBmanager {
+
+
+    private URL mongoDBurl;
+    private String pwd;
+
+    public DBmanager() {
+        initializeDB()
+    }
+
+    private boolean initializeDB() {
+        return true;
+    }
+
+    /**
+     * Get a connection to the MongoDB database
+     * @return a MongoDB database connection
+     */
+    public DBmanager getDatabase() {
+
+    }
+}
