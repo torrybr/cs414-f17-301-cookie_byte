5b898fc2f4a4f625c313b91b06a5accd5354d19e
torry brelsford <torrybr@rams.colostate.edu>
2017-10-16 11:00:11 -0600

    updated jar

--- p3/cs414-f17-301-cookie_byte-Application/src/DatabaseManagerImpl.java
+++ p3/cs414-f17-301-cookie_byte-Application/src/DatabaseManagerImpl.java
@@ -2,11 +2,7 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
 
-import java.net.URL;
-
 public class DatabaseManagerImpl {
-
-
     private MongoClientURI uri = new MongoClientURI(
             "mongodb://cs414team:<GO_CSU_rams_2019!>@cluster0-shard-00-00-u3hx4.mongodb.net:27017,cluster0-shard-00-01-u3hx4.mongodb.net:27017,cluster0-shard-00-02-u3hx4.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
 
@@ -20,10 +16,17 @@ public class DatabaseManagerImpl {
      * @return
      */
     private boolean initializeDB() {
+        MongoClient mongoClient = new MongoClient(uri);
+        MongoDatabase database = mongoClient.getDatabase("cs414Application");
+        System.out.println(database.getName());
         return true;
     }
 
 
-    MongoClient mongoClient = new MongoClient(uri);
-    MongoDatabase database = mongoClient.getDatabase("test");
+
+    public static void main(String [] args)
+    {
+        DatabaseManagerImpl newDB = new DatabaseManagerImpl();
+        newDB.initializeDB();
+    }
 }
