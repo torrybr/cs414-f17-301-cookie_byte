09dee1efc92f2c52b66e6072306aa65d8dec3f43
torry brelsford <torrybr@rams.colostate.edu>
2017-10-16 11:31:34 -0600

    helpul methods for database interaction

--- p3/cs414-f17-301-cookie_byte-Application/src/DatabaseManagerImpl.java
+++ p3/cs414-f17-301-cookie_byte-Application/src/DatabaseManagerImpl.java
@@ -1,10 +1,14 @@
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+
+import java.util.List;
 
 public class DatabaseManagerImpl {
     private MongoClientURI uri = new MongoClientURI(
             "mongodb://cs414team:<GO_CSU_rams_2019!>@cluster0-shard-00-00-u3hx4.mongodb.net:27017,cluster0-shard-00-01-u3hx4.mongodb.net:27017,cluster0-shard-00-02-u3hx4.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
+    MongoClient mongoClient = new MongoClient(uri);
 
     public DatabaseManagerImpl() {
         initializeDB();
@@ -16,17 +20,28 @@ public class DatabaseManagerImpl {
      * @return
      */
     private boolean initializeDB() {
-        MongoClient mongoClient = new MongoClient(uri);
-        MongoDatabase database = mongoClient.getDatabase("cs414Application");
-        System.out.println(database.getName());
         return true;
     }
 
+    /**
+     * @param databaseName the database you want access to. ex. "cs414team"
+     * @return MongoDatabase object so you can have access to database methods
+     */
+    public MongoDatabase getDatabase(String databaseName) {
+        MongoDatabase database = mongoClient.getDatabase(databaseName);
+        return database;
+    }
 
-
-    public static void main(String [] args)
-    {
+    public static void main(String[] args) {
         DatabaseManagerImpl newDB = new DatabaseManagerImpl();
-        newDB.initializeDB();
+    }
+
+    /**
+     * Get all the databases available
+     *
+     * @return list of database names.
+     */
+    public MongoIterable<String> getAllDatabases() {
+        return mongoClient.listDatabaseNames();
     }
 }
