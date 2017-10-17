838b9a33d89d5726027167dda6e8b86c1e4555ba
torry brelsford <torrybr@rams.colostate.edu>
2017-10-16 19:21:05 -0600

    createNewUser method

--- p3/cs414-f17-301-cookie_byte-Application/src/DatabaseManagerImpl.java
+++ p3/cs414-f17-301-cookie_byte-Application/src/DatabaseManagerImpl.java
@@ -9,18 +9,27 @@ import javax.xml.ws.http.HTTPException;
 
 
 public class DatabaseManagerImpl {
+
+    /* The location of the remote Database. */
     private MongoClientURI uri;
+
+    /* The mongoDB client. */
     private MongoClient mongoClient;
+
+    /* Logs system exceptions */
     final static Logger log = Logger.getLogger(DatabaseManagerImpl.class);
 
+    /**
+     * Connections to the MongoDatabase
+     */
     public DatabaseManagerImpl() {
         initializeDB();
     }
 
     /**
-     * initializes the MongoDB, creates the database if it is not present
+     * Initializes the MongoDB, creates the database if it is not present and sets the Connection string so its available to other methods in the class.
      *
-     * @return
+     * @return true or false if the connection to the database was successful
      */
     private boolean initializeDB() {
         try {
@@ -28,32 +37,26 @@ public class DatabaseManagerImpl {
                     "mongodb://app:tsM6N8irlTgHNzMa@cluster0-shard-00-00-u3hx4.mongodb.net:27017,cluster0-shard-00-01-u3hx4.mongodb.net:27017,cluster0-shard-00-02-u3hx4.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
             mongoClient = new MongoClient(uri);
             return true;
-
         } catch (HTTPException e) {
             log.error("Could not connect to the Database ", e);
             return false;
         }
-
-
     }
 
     /**
-     * Method to insert a NEW user.
-     * @param nickname the unique name a user is identifed by
-     * @param email the unique email a user uses to recieve notifications
-     * @param password Pass me a PRE-HASHED String that gets stored in the DB
+     * Method to insert a NEW user. This method does not valid input. It takes the exact string and inputs into DB.
+     *
+     * @param nickname the unique name a user is identified by
+     * @param email    the unique email a user uses to receive notifications
+     * @param password Pass me a PRE-HASHED String
      */
-    public void insertNewUser(String nickname, String email, String password) {
+    public void createNewUser(String nickname, String email, String password) {
         MongoDatabase db = mongoClient.getDatabase("cs414Application");
         MongoCollection<Document> collection = db.getCollection("users");
 
         Document user = new Document("nickname", nickname)
-                                    .append("email",email)
-                                    .append("password",password);
+                .append("email", email)
+                .append("password", password);
         collection.insertOne(user);
-    }
-
-    public static void main(String[] args) {
-        DatabaseManagerImpl newDB = new DatabaseManagerImpl();
     }
 }
