f4a9a44d2c6ed3150f11417fc7346a7a80e8ba93
Mauricio Munoz <mauriciomunozsc@gmail.com>
2017-10-18 16:36:22 -0600

    adding psudocode for move

--- p3/cs414-f17-301-cookie_byte-Application/src/Backend/Move.java
+++ p3/cs414-f17-301-cookie_byte-Application/src/Backend/Move.java
@@ -1,16 +1,61 @@
-package Backend;
-
 public class Move {
 
-	 User Player;
+	 User player;
 	 Game game;
-	 Piece Piece;
+	 Piece piece;
 	 int newLocation;
 	 
-	 public Move (User Player,Piece Piece, int newLocation){
-		 this.Player = Player;
-		 this.Piece=Piece;
+	 public Move (User player,Piece piece, int newLocation){
+		 this.Player = player;
+		 this.Piece= piece;
 		 this.newLocation = newLocation;
+	 }
+	 
+	
+	 public boolean checkValid(User player, Piece piece, int newLocation) {	 
+		 //if checkTurn() is false 
+		 	//return false 
+		 
+		 //if checkPiece() is false
+		 	//return false 
+		 
+		 //return true
+	 }
+	 
+	 private boolean checkTurn(User player) {
+		 //if it is not users turn 
+		 	//return false 
+		 //else 
+		 	//return true
+	 }
+	 
+	 private boolean checkPiece(Piece piece, int newLocation) {
+		 //Step 1: get piece location
+		 
+		 //Step 2: compare the x and y coordinated of the old location and the new location 
+		 	//if both x and y changed 
+		 		//return false
+		 	//if new location is out of bounds 
+		 		//return false 
+		 	//if there is already a piece at new location
+		 		//return false 
+		 	//if x or y changed by more than 1 
+		 		//if change of x or y is 2 
+		 			//if change-1 is middle square 
+		 				//continue
+		 			//else
+		 				//return false 
+		 	//if new location is refugee square
+		 		//if piece is not king 
+		 			//return false 
+		 	
+		 
+		 //return true
+	 }
+	 
+	 public boolean updateStatus() {
+		 
+		 
 	 }
 	
 	 
