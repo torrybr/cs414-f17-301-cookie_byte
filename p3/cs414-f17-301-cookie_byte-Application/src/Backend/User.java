3c356665897c54e2043f23a3d7ed5a8bf4541013
Mauricio Munoz <mauriciomunozsc@gmail.com>
2017-10-30 17:11:13 -0600

    comments for code of what will be written inside of user.

--- p3/cs414-f17-301-cookie_byte-Application/src/Backend/User.java
+++ p3/cs414-f17-301-cookie_byte-Application/src/Backend/User.java
@@ -1,14 +1,40 @@
 package Backend;
 public class User {
-
-	String userID;
 	
 // A Constructor for whenever a new user registers into our system and needs to be created
-	public User (String userID){
-		
+	//What we want user to do: have a USER ID 
+	//List of invites 
+	//List of pastGames  
+	//List of currentGames 
+	//int for win and for loss or tie 
+	String userID; //unique nickname (userID)  
+	String password; //password  
+	String email; //unique email 
+	
+	
+	public User (String userID, String password, String email)
+	{
 		this.userID = userID;
-
+		this.password = password; 
+		this.email = email; 
+		
 	}
+	
+	//make sure that userID is unique 
+	//make sure that email is unique 
+	
+	//get invites
+	//add to invites 
+	
+	//get PastGames
+	//add to pastGames 
+	//get currentGames
+	//add to currentGames
+	
+	//add to loss count
+	//get loss count 
+	//add to win count 
+	//get win count 
 	
 }
 
