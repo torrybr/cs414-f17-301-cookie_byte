c0a2ae7721e8a68aff9c5afb57475a3dba536549
Mauricio Munoz <mauricio.pelco@gmail.com>
2017-10-31 10:30:17 -0600

    adding a test package for cleaner look

---
+++ p3/cs414-f17-301-cookie_byte-Application/src/BackendTest/TestUser.java
@@ -0,0 +1,14 @@
+package BackendTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestUser {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
