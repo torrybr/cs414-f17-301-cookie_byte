51e97992314744e287f078472382be7339af17f6
Mauricio Munoz <mauricio.pelco@gmail.com>
2017-10-31 11:15:37 -0600

    Testing adding one invite to user

--- p3/cs414-f17-301-cookie_byte-Application/src/BackendTest/UserTest.java
+++ p3/cs414-f17-301-cookie_byte-Application/src/BackendTest/UserTest.java
@@ -4,17 +4,32 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Backend.Invite;
 import Backend.User;
 
 public class UserTest {
 
 	@Test
 	//testing constructor
-	public void testUser() {
+	public void testUser()
+	{
 		User u = new User("A", "B", "C");
 		assertEquals("A" , u.getUserID());
 		assertEquals("B" , u.getPassword());
 		assertEquals("C" , u.getEmail());
+	}
+	
+	@Test
+	public void testOneAddInvite()
+	{
+		User u = new User("A", "B", "C");
+		User u2 = new User("D", "E", "F");
+		Invite i = new Invite(u, u2, 0);
+		u.addInvite(i);
+		for(int j = 0; j < u.getInvites().size(); j++) 
+		{
+		assertEquals(i, u.getInvites().get(j));
+		}
 	}
 
 }
