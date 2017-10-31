47c1bcd6b39801f24842fbaf91d75d89d7dea431
Mauricio Munoz <mauricio.pelco@gmail.com>
2017-10-31 10:11:38 -0600

    getter and adder classes for User class

--- p3/cs414-f17-301-cookie_byte-Application/src/Backend/User.java
+++ p3/cs414-f17-301-cookie_byte-Application/src/Backend/User.java
@@ -1,4 +1,8 @@
 package Backend;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 	
 // A Constructor for whenever a new user registers into our system and needs to be created
@@ -10,6 +14,12 @@ public class User {
 	String userID; //unique nickname (userID)  
 	String password; //password  
 	String email; //unique email 
+	private List<Invite> invites = new ArrayList<Invite>();
+	private List<GameController> pastGames = new ArrayList<GameController>();
+	private List<GameController> currentGames = new ArrayList<GameController>();
+	private int wins;
+	private int losses;
+	
 	
 	
 	public User (String userID, String password, String email)
@@ -20,18 +30,47 @@ public class User {
 		
 	}
 	
-	//make sure that userID  
-	//make sure that email  
-	
 	//get invites: getInvites() 
+	public List<Invite> getInvites()
+	{
+		return invites;
+	}
 	//add to invites: addInvite() 
+	public void addInvite(Invite i)
+	{
+		invites.add(i);
+	}
 	//remove from invites: removeInvite() 
+	public void removeInvite(Invite i)
+	{
+		invites.remove(i);
+	}
 	
-	//get pastGames: getPastGames()  
+	//get pastGames: getPastGames()
+	public List<GameController> getPastGames()
+	{
+		return pastGames;
+	}
 	//add to pastGames: addPastGame()
+	public void addPastGame(GameController g)
+	{
+		pastGames.add(g);
+	}
 	//get currentGames: getCurrentGames()
+	public List<GameController> getCurrentGames()
+	{
+		return currentGames;
+	}
 	//add to currentGames: addCurrentGame()
-	//remove from current games: removeCurrentGames()
+	public void addCurrentGame(GameController g)
+	{
+		currentGames.add(g);
+	}
+	//remove from current games: removeCurrentGame()
+	public void removeCurrentGame(GameController g)
+	{
+		currentGames.remove(g);
+	}
 	
 	//add to loss count: addLoss()
 	//get loss count: getLosses() 
