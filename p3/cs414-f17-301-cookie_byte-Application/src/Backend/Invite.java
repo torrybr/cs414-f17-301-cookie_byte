b1757c8ff9fd25e18c1570dc1ba9f4a430541986
benbaranovsky <benbaranovsky@gmail.com>
2017-11-08 16:17:53 -0700

    After invite accepted, backend no longer adds to currentGames of users
    I believe this is done in the DB and so I removed it from Invite due to
    duplicated. Already spoke with Torry about it

--- p3/cs414-f17-301-cookie_byte-Application/src/Backend/Invite.java
+++ p3/cs414-f17-301-cookie_byte-Application/src/Backend/Invite.java
@@ -27,9 +27,9 @@ public class Invite {
 		GameController gme = new GameController(gameID, userTo, userFrom);
 		// Sets game to active
 		gme.setStatus(GameStatus.ACTIVE);
-		// Add game to both users
-		userTo.addCurrentGame(gme);
-		userFrom.addCurrentGame(gme);
+		// Add game to both users THIS IS NOW DONE IN DB
+		// userTo.addCurrentGame(gme);
+		// userFrom.addCurrentGame(gme);
 		// Remove invite from receiving user
 		userTo.removeInvite(this);//////////////////
 	}
