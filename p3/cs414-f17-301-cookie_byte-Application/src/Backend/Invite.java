923668e0fc14c00abbc38f9888a031c5f9d88550
benbaranovsky <benbaranovsky@gmail.com>
2017-11-08 16:52:25 -0700

    Removing hashCode from Invite

--- p3/cs414-f17-301-cookie_byte-Application/src/Backend/Invite.java
+++ p3/cs414-f17-301-cookie_byte-Application/src/Backend/Invite.java
@@ -41,14 +41,6 @@ public class Invite {
 		// Remove invite from receiving user
 		userTo.removeInvite(this);
 	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + gameID;
-		return result;
-	}
 
 	@Override
 	public boolean equals(Object obj) {
