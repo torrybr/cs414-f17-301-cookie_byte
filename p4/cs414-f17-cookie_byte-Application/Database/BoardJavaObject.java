3d93cc185798aeecdbee79f9d5899994b9890249
Timothy Chesson <tchesson@rams.colostate.edu>
2017-11-12 12:31:29 -0700

    Made a new p4 project folder for our Application

---
+++ p4/cs414-f17-cookie_byte-Application/Database/BoardJavaObject.java
@@ -0,0 +1,109 @@
+
+package Database;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "_id",
+    "GameID",
+    "Player1",
+    "Player2",
+    "CurrentTurn",
+    "Board"
+})
+public class BoardJavaObject {
+
+    @JsonProperty("_id")
+    private Id id;
+    @JsonProperty("GameID")
+    private Integer gameID;
+    @JsonProperty("Player1")
+    private String player1;
+    @JsonProperty("Player2")
+    private String player2;
+    @JsonProperty("CurrentTurn")
+    private String currentTurn;
+    @JsonProperty("Board")
+    private Board board;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("_id")
+    public Id getId() {
+        return id;
+    }
+
+    @JsonProperty("_id")
+    public void setId(Id id) {
+        this.id = id;
+    }
+
+    @JsonProperty("GameID")
+    public Integer getGameID() {
+        return gameID;
+    }
+
+    @JsonProperty("GameID")
+    public void setGameID(Integer gameID) {
+        this.gameID = gameID;
+    }
+
+    @JsonProperty("Player1")
+    public String getPlayer1() {
+        return player1;
+    }
+
+    @JsonProperty("Player1")
+    public void setPlayer1(String player1) {
+        this.player1 = player1;
+    }
+
+    @JsonProperty("Player2")
+    public String getPlayer2() {
+        return player2;
+    }
+
+    @JsonProperty("Player2")
+    public void setPlayer2(String player2) {
+        this.player2 = player2;
+    }
+
+    @JsonProperty("CurrentTurn")
+    public String getCurrentTurn() {
+        return currentTurn;
+    }
+
+    @JsonProperty("CurrentTurn")
+    public void setCurrentTurn(String currentTurn) {
+        this.currentTurn = currentTurn;
+    }
+
+    @JsonProperty("Board")
+    public Board getBoard() {
+        return board;
+    }
+
+    @JsonProperty("Board")
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ p4/cs414-f17-cookie_byte-Application/UI/CreateGame.java
@@ -0,0 +1,115 @@
+package UI;
+
+import Backend.GameController;
+import Drivers.ClientDriver;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class CreateGame  extends Application {
+	protected String sender;
+	protected String reciever;
+	protected Stage main; 
+	public ClientDriver client;
+	
+	public CreateGame(ClientDriver driver){
+	 	this.client = driver;
+	}
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		main = primaryStage;
+		BorderPane border = new BorderPane();
+		border.setTop(addHBox());
+		border.setLeft(addVBox("Create game with....","player1"));
+		Scene scene = new Scene(border,635,375);
+		primaryStage.setTitle("Login");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	
+	}
+
+	public VBox addVBox(String label,String sub1) {
+	    VBox vbox = new VBox();
+	    vbox.setPadding(new Insets(10));
+	    vbox.setSpacing(8);
+
+	    Text title = new Text(label);
+	    title.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+	    vbox.getChildren().add(title);
+
+	    Button options[] = new Button[] {
+	        new Button(sub1)};
+	    
+	    options[0].setOnAction(new EventHandler<ActionEvent>() {
+       	 
+            @Override
+            public void handle(ActionEvent e) {
+            		reciever = options[0].getText();
+            		send();
+            }
+        });
+	    
+	    
+	    VBox.setMargin(options[0], new Insets(0, 0, 0, 8));
+	    vbox.getChildren().add(options[0]);
+
+	    return vbox;
+	}
+	
+	public HBox addHBox() {
+	    HBox hbox = new HBox();
+	    hbox.setPadding(new Insets(15, 12, 15, 12));
+	    hbox.setSpacing(10);
+	    hbox.setStyle("-fx-background-color: #336699;");
+
+	    Button buttonHome = new Button("Home");
+	    buttonHome.setPrefSize(100, 20);
+
+	    buttonHome.setOnAction(new EventHandler<ActionEvent>() {
+       	 
+            @Override
+            public void handle(ActionEvent e) {
+            		Home home = new Home(client);
+            		try {
+						home.start(main);
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+                
+            }
+        });
+	  
+	    
+	    final Pane spacer = new Pane();
+	    HBox.setHgrow(spacer, Priority.ALWAYS);
+	    spacer.setMinSize(10, 1);
+	    hbox.getChildren().addAll(spacer,buttonHome);
+
+	    return hbox;
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void send() {
+		//create a new game with those sender reciever as player 1 player 2. How to create unique game ID?
+		//GameController gameDriver = new GameController(sender,reciever);
+		//Game game = new Game(client,gameDriver);
+		try {
+		//	game.start(main);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
